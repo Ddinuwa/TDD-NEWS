d5aca77dde79493ca37ef6da49db9d4ade5e7ea1
HESHAN-MD <ddinuwa83@gmail.com>
2025-01-06 00:43:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
-    OWNER_NUMBER: '94711453361',
+    SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=XR8QQByQ#CJJiiNGmwIhP2B1iCYrdc1YdroL1_iYXGEvZnQ6PRAA' : process.env.SESSION_ID,
+    OWNER_NUMBER: '94767748006',
    // Put news sender jid below you need to send news
     hiru: '120363190669566051@g.us',
     lankadeepa: '120363190669566051@g.us',
