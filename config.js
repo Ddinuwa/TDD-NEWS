047cc7ad1ca90c3c6c5bddc1f9985e2d68e4494f
HESHAN-MD <ddinuwa83@gmail.com>
2025-01-06 08:13:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,13 +8,13 @@ module.exports = {
     SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=XR8QQByQ#CJJiiNGmwIhP2B1iCYrdc1YdroL1_iYXGEvZnQ6PRAA' : process.env.SESSION_ID,
     OWNER_NUMBER: '94767748006',
    // Put news sender jid below you need to send news
-    hiru: '120363190669566051@g.us',
-    lankadeepa: '120363190669566051@g.us',
-    itn: '120363190669566051@g.us',
-    bbc: '120363190669566051@g.us',
-    gossiplankanews: '120363190669566051@g.us',
-    dasathalankanews: '120363190669566051@g.us',
-    siyatha: '120363190669566051@g.us',
-    derana: '120363190669566051@g.us'
+    hiru: '120363201439891862@g.us',
+    lankadeepa: '120363201439891862@g.us',
+    itn: '120363201439891862@g.us',
+    bbc: '120363201439891862@g.us',
+    gossiplankanews: '120363201439891862@g.us',
+    dasathalankanews: '120363201439891862@g.us',
+    siyatha: '120363201439891862@g.us',
+    derana: '120363201439891862@g.us'
 };
 
